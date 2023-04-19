37e5ea69e3c6c508992c68718448b1a5a33999d7
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2023-04-19 22:39:19 +0200

    Add project files.

---
+++ webApi/webApi.DAL/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace webApi.webApi.DAL
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Price { get; set; }
+
+        public string? Title { get; set; }
+    }
+}
---
+++ webApi/webApi.DAL/ProductContex.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace webApi.webApi.DAL
+{
+    public class ProductContex : DbContext
+    {
+        public ProductContex(DbContextOptions < ProductContex > options): base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
